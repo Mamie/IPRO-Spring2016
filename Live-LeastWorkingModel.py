297ff6690f67685a0966f82a15d7277b86fcdf90
bgrimmer <bgrimmer@hawk.iit.edu>
2016-02-15 01:38:38 -0600

    Added code to populate graph data structure and compute smile string for non-cyclic molecules

--- Live-LeastWorkingModel.py
+++ Live-LeastWorkingModel.py
@@ -48,11 +48,10 @@ def nearestNeighbor(colors, pixel):
 
 def detectSingleBond(image, c1, c2):
 	distance = distance2D(c1,c2)
-	print distance, c1, c2
 	if distance>5*c1[2] or distance>5*c2[2]: return False
-	STEPS =20
+	STEPS =25
 	PERCENT_SKIP=0.3
-	THRESHOLD=0.85
+	THRESHOLD=0.90
 	p1 = c1*PERCENT_SKIP + c2*(1-PERCENT_SKIP)
 	p2 = c2*PERCENT_SKIP + c1*(1-PERCENT_SKIP)
 	direction = p2-p1
@@ -60,13 +59,30 @@ def detectSingleBond(image, c1, c2):
 	for i in range(STEPS):
 		step = direction*i/STEPS
 		test = p1 + step
-		#print 'Sampling'
-		#print test, step
-		#print image[test[1]][test[0]]
-		if abs(distance3D(image[math.floor(test[1])][math.floor(test[0])], [240,250,240]))<50:
+		if abs(distance3D(image[math.floor(test[1])][math.floor(test[0])], [240,250,240]))<75:
 			count = count+1
 	return count>THRESHOLD
 #End Function detectSingleBond()
+
+def DFS(graph, vertexNames):
+	if graph==None or graph==[]: return None
+	start = 0
+	while len(graph[start])==0:
+		start=start+1
+		if start==len(graph): return None
+	ret=''
+	seen = [False]*len(graph)
+	stack = [start]
+	while not len(stack)==0:
+		cur = stack.pop()
+		if seen[cur]: continue
+		print cur
+		seen[cur]=True
+		for i in range(len(graph)):
+			if i in graph[cur] and not seen[i]:
+				stack.append(i)
+	return ret
+#End Function DFS(graph, vertexNames)
 
 cap = cv2.VideoCapture(0)
 cv2.namedWindow('Ball And Stick Tracker',cv2.CV_WINDOW_AUTOSIZE)
@@ -74,30 +90,37 @@ cv2.namedWindow('Ball And Stick Tracker',cv2.CV_WINDOW_AUTOSIZE)
 
 printHelpText()
 
+
 quit=False
 while not quit:
 	retval,image = cap.read()
 	cv2.imshow('Ball And Stick Tracker', image)
 	key = cv2.waitKey()
 	while True:
+		vertexNames = None
+		graph = None
+
 		retval,image = cap.read()
 		image = cv2.medianBlur(image,3)
 		cimg = image.copy()
 		img = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-		circles = cv2.HoughCircles(img,cv2.cv.CV_HOUGH_GRADIENT,1,20,
-                       			    param1=45,param2=25,minRadius=10,maxRadius=50)
+		circles = cv2.HoughCircles(img,cv2.cv.CV_HOUGH_GRADIENT,1,30,
+                       			    param1=45,param2=25,minRadius=15,maxRadius=50)
 		if circles!=None:
 			circles = np.uint16(np.around(circles))
+			atomNames = ['C', 'O', 'N']
 			colorNames = ['Black', 'Red', 'Blue']
 			color = [[45,45,45], [130,160,240], [255,240,180]]#[[45,45,45], [50,55,90], [105,75,60]]
 			count = [0,0,0]
 
-			print 'Found circles'
+			vertexNames = []
+			graph = []
 			for i in circles[0,:]:
 				circleColor = averageColor(image, i[1], i[0], 1)
-				print image[i[1]][i[0]], circleColor
 				color_index = nearestNeighbor(color, circleColor)
 				count[color_index]=count[color_index]+1
+				vertexNames.append(atomNames[color_index])
+
 				# draw the outer circle
 				cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
 				# draw the center of the circle
@@ -107,21 +130,38 @@ while not quit:
 
 			#Mark Connections
 			for i in circles[0,:]:
+				adjList = []
+				counter=0
 				for j in circles[0,:]:
+					if (i==j).all():
+						counter=counter+1
+						continue
 					if detectSingleBond(image,i,j):
+						adjList.append(counter)
 						cv2.line(cimg,(i[0],i[1]),(j[0],j[1]),(255,0,0),3)
 					#End if
+					counter=counter+1
 				#End for
+				graph.append(adjList)
 			#End for
-			print count
+			#print vertexNames
+			#print count
+			#print graph
 		#End If
 
 		cv2.imshow('Ball And Stick Tracker', cimg)
-		key = cv2.waitKey(20)
+		key = cv2.waitKey(200)
 		if key==-1: continue
 		if chr(key)=='q': 
 			quit=True
 			break
+		elif chr(key)=='s':
+			print 's was pressed'
+			print vertexNames
+			print graph
+			smile = DFS(graph, vertexNames)
+			print smile
+			print ''
 		elif chr(key)=='p':
 			break
 		#End While
